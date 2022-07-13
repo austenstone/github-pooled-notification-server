7a8b3c3fc7236a70691f5b9f10e7d63b33eda987
Austen Stone <austenstone@github.com>
2022-07-12 21:09:06 -0400

    app

--- communication/github.ts
+++ communication/github.ts
@@ -1,10 +1,12 @@
+import { create } from "https://deno.land/x/djwt/mod.ts";
 
 const GH_API_URL = "https://api.github.com";
 export class GitHub {
-  token: string;
+  jwt?: string;
+  installationId?: number;
+  token?: string;
 
-  constructor(token: string) {
-    this.token = token;
+  constructor() {
   }
 
   request(method: string, url: string, body?: any, init?: RequestInit) {
@@ -19,9 +21,88 @@ export class GitHub {
     });
   }
 
+  bearerRequest(method: string, url: string, body?: any, init?: RequestInit) {
+    return fetch(GH_API_URL + url, {
+      method,
+      body: JSON.stringify(body),
+      headers: {
+        "Authorization": `Bearer ${this.jwt}`,
+        "Accept": "application/vnd.github+json"
+      },
+      ...init
+    });
+  }
+
   issueUpdateAssignees(owner: string, repo: string, issueNumber: number, assignees: string[]) {
     return this.request('POST', `/repos/${owner}/${repo}/issues/${issueNumber}/assignees`, {
       assignees
     });
   }
+
+  async initalize(appId: string) {
+    await this.createJwt(appId);
+  
+    const installationsRsp = await this.getInstallations();
+    const installations = await installationsRsp.json();
+    await this.setInstallationId(installations.find(i => i).id);
+    await this.getAccessToken();
+  }
+
+  getInstallationRepositories() {
+    return this.request('GET', '/installation/repositories');
+  }
+
+  getInstallations() {
+    return this.bearerRequest('GET', '/app/installations');
+  }
+
+  setInstallationId(installationId: number) {
+    this.installationId = installationId;
+  }
+
+  async getAccessToken(): Promise<void> {
+    const rsp = await this.bearerRequest('POST', `/app/installations/${this.installationId}/access_tokens`);
+    const accessTokens = await rsp.json();
+    this.token = accessTokens.token;
+  }
+
+  // The private keys provided by GitHub are in PKCS#1 format, but the WebCrypto API only supports PKCS#8.
+  // You need to convert it first:
+  // $ openssl pkcs8 -topk8 -inform PEM -outform PEM -nocrypt -in private-key.pem -out private-key-pkcs8.key
+  async createJwt(appId: string) {
+    const str2ab = (str: string) => {
+      const buf = new ArrayBuffer(str.length);
+      const bufView = new Uint8Array(buf);
+      for (let i = 0, strLen = str.length; i < strLen; i++) {
+        bufView[i] = str.charCodeAt(i);
+      }
+      return buf;
+    }
+    const pem = await Deno.env.get('GITHUB_APP_PRIVATE_KEY');
+    if (!pem) throw new Error('GITHUB_APP_PRIVATE_KEY not set');
+    const pemHeader = "-----BEGIN PRIVATE KEY-----";
+    const pemFooter = "-----END PRIVATE KEY-----";
+    const pemContents = pem.substring(
+      pemHeader.length,
+      pem.length - pemFooter.length,
+    );
+    // base64 decode the string to get the binary data
+    const binaryDerString = atob(pemContents);
+    // convert from a binary string to an ArrayBuffer
+    const binaryDer = str2ab(binaryDerString);
+    const key = await crypto.subtle.importKey(
+      "pkcs8",
+      binaryDer,
+      { name: "RSASSA-PKCS1-v1_5", hash: { name: "SHA-256" } },
+      true,
+      ["sign"]
+    );
+    const nowSeconds = Math.round(Date.now() / 1000);
+    this.jwt = await create({ alg: "RS256", typ: "JWT" }, {
+      iat: nowSeconds - 60,
+      exp: nowSeconds + (10 * 60),
+      iss: appId
+    }, key);
+  }
+
 }
--- routes/slack/interactive-endpoint.ts
+++ routes/slack/interactive-endpoint.ts
@@ -2,38 +2,8 @@ import { HandlerContext } from "$fresh/server.ts";
 import { GitHub } from "../../communication/github.ts";
 import { SlackRequest } from "../../communication/slack-types.ts";
 import moment from "https://deno.land/x/momentjs@2.29.1-deno/mod.ts";
-import { create } from "https://deno.land/x/djwt/mod.ts";
-import * as base64 from "https://deno.land/std@0.140.0/encoding/base64.ts";
 
-
-function str2ab(str) {
-  const buf = new ArrayBuffer(str.length);
-  const bufView = new Uint8Array(buf);
-  for (let i = 0, strLen = str.length; i < strLen; i++) {
-    bufView[i] = str.charCodeAt(i);
-  }
-  return buf;
-}
-
-const pem = await Deno.readTextFileSync("./private-key.pem");
-const pemHeader = "-----BEGIN RSA PRIVATE KEY-----\n";
-const pemFooter = "\n-----END RSA PRIVATE KEY-----";
-const pemContents = pem.substring(
-  pemHeader.length,
-  pem.length - pemFooter.length,
-);
-// base64 decode the string to get the binary data
-const binaryDerString = atob(pemContents);
-// convert from a binary string to an ArrayBuffer
-const binaryDer = str2ab(binaryDerString);
-console.log('pemContents', pemContents);
-const key = await crypto.subtle.importKey("raw", binaryDer, { name: "RSA-PSS", hash: "SHA-256" }, true, ["sign"]);
-// const jwt = await create({ alg: "RS256", typ: "JWT" }, {
-//   iat: Date.now() - 60,
-//   exp: Date.now() + (10 * 60),
-//   iss: "YOUR_APP_ID"
-// }, key);
-// console.log(jwt);
+const appId = '219392'; // https://github.com/apps/pooled-notifications
 
 const owner = 'austenstone';
 const repo = 'pooled-notif-test';
@@ -41,7 +11,6 @@ const token = Deno.env.get("GITHUB_TOKEN");
 if (!token) {
   Deno.exit(1);
 }
-const client = new GitHub(token);
 
 const getReqData = async (req: Request) => {
   const text = await req.text();
@@ -60,20 +29,23 @@ export const handler = async (_req: Request, _ctx: HandlerContext): Promise<Resp
 
   if (isNaN(issueNumber)) throw new Error('Invalid issue number: ' + data.message.text);
 
+  const client = new GitHub();
+  await client.initalize(appId);
+
   client.issueUpdateAssignees(owner, repo, issueNumber, [data.user.username]).then(async (rsp) => {
     const rspData = await rsp.json();
     const createdAt = new Date(rspData.created_at);
     const updatedAt = new Date(rspData.updated_at);
     const duration = Math.abs(updatedAt - createdAt);
     const durationStr = moment.duration(duration).humanize();
-    // fetch(data.response_url, {
-    //   method: 'POST',
-    //   body: JSON.stringify({
-    //     "replace_original": "true",
-    //     text: `Pooled issue <${htmlUrl}|${issueTitle} #${issueNumber}> assigned to <https://github.com/${data.user.username}|@${data.user.username}> ${durationStr} after creation.`
-    //   }),
-    //   headers: { "Content-Type": "application/json" }
-    // }).then((rsp) => console.debug('<- github', rsp));
+    fetch(data.response_url, {
+      method: 'POST',
+      body: JSON.stringify({
+        "replace_original": "true",
+        text: `Pooled issue <${htmlUrl}|${issueTitle} #${issueNumber}> assigned to <https://github.com/${data.user.username}|@${data.user.username}> ${durationStr} after creation.`
+      }),
+      headers: { "Content-Type": "application/json" }
+    }).then((rsp) => console.debug('<- github', rsp));
   }).catch((e) => {
     throw new Error(e.message ? e.message : JSON.stringify(e));
   });
